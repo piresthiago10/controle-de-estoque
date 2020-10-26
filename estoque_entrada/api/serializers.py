d0828ecb41b777ba390f53a4062cc5da98ef6bf4
Thiago Pires <thiago.pires@sectrans.com.br>
2020-10-26 15:08:01 -0300

    feature - sobrescrita create

--- estoque_entrada/api/serializers.py
+++ estoque_entrada/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from produto.api.serializers import ProdutoSerializer
 from estoque_entrada.models import EstoqueEntrada
 
 
+
 class EstoqueEntradaSerializer(ModelSerializer):
+    # produto = ProdutoSerializer()
+
     class Meta:
         model = EstoqueEntrada
         fields = ('id', 'produto', 'data', 'quantidade_entrada', 'valor_pago', 'usuario')
