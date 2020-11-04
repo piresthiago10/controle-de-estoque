af6575c8986656e6b9fdff0cabb3a6f58d231557
Thiago Pires <thiago.pires@sectrans.com.br>
2020-11-04 14:40:38 -0300

    hotfix - aruivos desnecessários

--- tipo_usuario/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import TipoUsuario
-
-admin.site.register(TipoUsuario)
--- tipo_usuario/api/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from tipo_usuario.models import TipoUsuario
-from .serializers import TipoUsuarioSerializer
-
-
-class TipoUsuarioViewSet(ModelViewSet):
-    queryset = TipoUsuario.objects.all()
-    serializer_class = TipoUsuarioSerializer
