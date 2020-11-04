5abe91ba7f0ce2775362a1b8d6158a80f4b2ec63
Thiago Pires <thiago.pires@sectrans.com.br>
2020-11-04 14:36:14 -0300

    hotfix - remoção de usuario customizado

--- estoque_entrada/models.py
+++ estoque_entrada/models.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 from produto.models import Produto
-from usuario.models import Usuario
+
 
 class EstoqueEntrada(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.SET('Produto descontinuado'))
+    produto = models.ForeignKey(
+        Produto, on_delete=models.SET('Produto descontinuado'))
     data = models.DateField()
     quantidade_entrada = models.IntegerField()
     valor_pago = models.DecimalField(max_digits=5, decimal_places=2)
-    usuario = models.ForeignKey(Usuario, on_delete=models.SET('Usuário Excluído'))
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.produto.nome
-
